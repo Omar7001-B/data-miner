41f9d193f73e84e1515ca119d38a600725291a24
Omar Abdelrahman Abbas <omar.abbas.7001@gmail.com>
2025-04-26 18:13:47 +0300

    Enable single data upload shared across all pages using Streamlit session state. Closes #2

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="DataMiner Home", layout="wide")
+st.title("Welcome to DataMiner!")
+st.write("""
+DataMiner is an interactive data mining and machine learning platform. Use the sidebar to navigate between different features:
+- Data Upload
+- Data Profiling
+- Summary Statistics
+
+More features will be added as the project grows!
+""") 
